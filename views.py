1137fc009b336ffde15be34e2cbb9bcf806b826e
Rodrigo Lopes Velloso <135156904+velloso99@users.noreply.github.com>
2025-11-29 19:47:21 -0300

    views

--- views.py
+++ views.py
@@ -33,9 +33,45 @@ def ver_bairro():
     return lista
 #Atualizar Bairro
 def atualizar_bairro(i):
+
     with con:
         cur = con.cursor()
         query = "UPDATE bairro SET BAIRRO=? WHERE id=?"
+        cur.execute(query, i)  # A variável 'dados' já é uma tupla
+        con.commit()
+
+        # Retorna True se a atualização for bem-sucedida
+        return cur.rowcount > 0
+    
+#######################################################################################################
+#Tabela Carteira de Clientes__________________________
+def criar_cliente(i):   
+    with con:
+        cur = con.cursor()
+        query = "INSERT INTO clientes (MATRICULA, RAZÃO SOCIAL, NOME FANTASIA, CNPJ, ENDREÇO, BAIRRO, CIDADE, ESTADO, ATENDENTE, TELEFONE) values(?,?,?,?,?,?,?,?,?,?)"
+        cur.execute(query, i)
+#Deletar Cliente
+def deletar_cliente(i):  
+    with con:
+        cur = con.cursor()
+        query = "DELETE FROM clientes WHERE id=? "
+        cur.execute(query, i)           
+#Ver Clientes
+def ver_cliente():  
+    lista = []
+    with con:
+        cur = con.cursor()
+        cur.execute('SELECT * FROM clientes')
+        linha = cur.fetchall()
+        
+        for i in linha:
+            lista.append(i)
+    return lista
+#Atualizar Cliente
+def atualizar_cliente(i):   
+    with con:
+        cur = con.cursor()
+        query = "UPDATE clientes SET MATRICULA=?, RAZÃO SOCIAL=?, NOME FANTASIA=?, CNPJ=?, ENDREÇO=?, BAIRRO=?, CIDADE=?, ESTADO=?, ATENDENTE=?, TELEFONE=? WHERE id=?"
         cur.execute(query, i)  # A variável 'dados' já é uma tupla
         con.commit()
 
